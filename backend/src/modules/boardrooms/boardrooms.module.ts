b6e0f4c61aafea64c215fa98f3eb3dafd51bc095
Carol Manthosa <carolmanthosa@gmail.com>
2026-05-26 20:25:29 +0200

    created boardroom entity and module

--- backend/src/modules/boardrooms/boardrooms.module.ts
+++ backend/src/modules/boardrooms/boardrooms.module.ts
@@ -5,7 +5,7 @@ import { Booking } from "../bookings/booking.entity";
 import { BoardroomBlock } from "../boardroom-blocks/boardroom-block.entity";
 import { Boardroom } from "./boardroom.entity";
 import { BoardroomsController } from "./boardrooms.controller";
-import { BoardroomsService } from "./boardrooms.service";
+import { BoardroomsService } from "./boardroom.entity";
 
 @Module({
   imports: [
